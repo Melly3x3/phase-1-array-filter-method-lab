1d9b8a2443163f71633e7b1a204d9fb1c44c524f
Melvin <jmelvin1134@gmail.com>
2023-05-06 17:46:48 -0400

    array filter complete

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-// Code your solution here
+function superbowlWin(){
+}
+
+function findMatching(names, string) {
+    const listOfDrivers = [];
+    for (const names of drivers) {
+        if (string === drivers)
+        {
+            return listOfDrivers.push(drivers)
+        }
+}
+    return names.filter(names => names.toUpperCase() === string.toUpperCase())
+}
+
+function fuzzyMatch(names, string,) {
+    return names.filter(names => names[0] === string[0]);
+}
+    
+function matchName(drivers, argument){
+    return  drivers.filter(function(drivers) { return drivers.name === argument});
+}
+const drivers =[
+    {
+        name: 'Bobby',
+        hometown: 'Pittsburgh'
+    },
+    {
+        name: 'Sammy',
+        hometown: 'New York'
+    },
+    {
+        name: 'Sally',
+        hometown: 'Cleveland'
+    },
+    {
+        name: 'Annette',
+        hometown: 'Los Angeles'
+    },
+    {
+        name: 'Bobby',
+        hometown: 'Tampa Bay'
+    },
+    ];
